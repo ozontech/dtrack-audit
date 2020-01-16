f77acd87963d91931492d89fd40ee136486acb87
Taras Ivashchenko <oxdef@ozon.ru>
2020-01-16 10:43:59 +0300

    Add env variable processing for sync mode option. Fix #5

--- cmd/dtrack-audit/main.go
+++ cmd/dtrack-audit/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ozonru/dtrack-audit/internal/dtrack"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -28,12 +29,18 @@ func main() {
 		fmt.Fprintf(flag.CommandLine.Output(), "\nFields marked with (*) are required.\n")
 	}
 
+	syncMode, err := strconv.ParseBool(os.Getenv("DTRACK_SYNC_MODE"))
+
+	if err != nil {
+		syncMode = false
+	}
+
 	flag.StringVar(&inputFileName, "i", "bom.xml", "Target SBOM file*")
 	flag.StringVar(&projectId, "p", os.Getenv("DTRACK_PROJECT_ID"), "Project ID*. Environment variable is DTRACK_PROJECT_ID")
 	flag.StringVar(&apiKey, "k", os.Getenv("DTRACK_API_KEY"), "API Key*. Environment variable is DTRACK_API_KEY")
 	flag.StringVar(&apiUrl, "u", os.Getenv("DTRACK_API_URL"), "API URL*. Environment variable is DTRACK_API_URL")
 	flag.StringVar(&severityFilter, "g", os.Getenv("DTRACK_SEVERITY_FILTER"), "With Sync mode enabled show result and fail an audit if the results include a vulnerability with a severity of specified level or higher. Severity levels are: critical, high, medium, low, info, unassigned. Environment variable is DTRACK_SEVERITY_FILTER")
-	flag.BoolVar(&syncMode, "s", false, "Sync mode enabled. That means: upload SBOM file, wait for scan result, show it and exit with non-zero code")
+	flag.BoolVar(&syncMode, "s", syncMode, "Sync mode enabled. That means: upload SBOM file, wait for scan result, show it and exit with non-zero code. Environment variable is DTRACK_SYNC_MODE")
 	flag.IntVar(&timeout, "t", 25, "Max timeout in second for polling API for project findings")
 	flag.Parse()
 
@@ -43,7 +50,7 @@ func main() {
 	}
 
 	apiClient := dtrack.ApiClient{ApiKey: apiKey, ApiUrl: apiUrl}
-	uploadResult, err := apiClient.Upload(inputFileName, projectId)
+	uploadResult, err = apiClient.Upload(inputFileName, projectId)
 	checkError(err)
 
 	if uploadResult.Token != "" {
