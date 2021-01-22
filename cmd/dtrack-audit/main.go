0e6ce40a32de0245fad0a8ab3738b77674e0ccff
Oleg Sorokin <osorokin@intermedia.net>
2021-01-22 17:04:37 +0300

    Add TeamCity support V2

--- cmd/dtrack-audit/main.go
+++ cmd/dtrack-audit/main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/ozonru/dtrack-audit/internal/dtrack"
 	"log"
 	"os"
-	"strconv"
 	"time"
 )
-
-type TeamCityMsg struct {
-	Time    time.Time
-	Action  string
-	Package string
-	Test    string
-	Output  string
-}
-
-const TeamCityPackageName = "github.com/ozonru/dtrack-audit/cmd/dtrack-audit"
-const TeamCityTestName = "TestVulnerabilities"
 
 func checkError(e error) {
 	if e != nil {
@@ -34,101 +22,58 @@ func formatFinding(f dtrack.Finding, apiClient dtrack.ApiClient) string {
 		f.Vuln.Severity, f.Vuln.Title, f.Comp.Name, f.Comp.Version, apiClient.GetVulnViewUrl(f.Vuln))
 }
 
-func printTeamCityMsg(action, output string) {
-	msg := TeamCityMsg{time.Now(), action, TeamCityPackageName, TeamCityTestName, output}
-	jsonData, err := json.Marshal(msg)
+func findVulnerabilities(apiClient dtrack.ApiClient, config *Config) (int, []dtrack.Finding, error) {
+	uploadResult, err := apiClient.Upload(config.inputFileName, config.projectId)
 	checkError(err)
-	fmt.Println(string(jsonData))
+	if uploadResult.Token != "" {
+		fmt.Printf("SBOM file is successfully uploaded to DTrack API. Result token is %s\n", uploadResult.Token)
+		err := apiClient.PollTokenBeingProcessed(uploadResult.Token, time.After(time.Duration(config.timeout)*time.Second))
+		checkError(err)
+		findings, err := apiClient.GetFindings(config.projectId, config.severityFilter)
+		checkError(err)
+		return len(findings), findings, err
+	}
+	return 0, nil, errors.New("token not found")
+}
+
+func findAndPrintForUser(apiClient dtrack.ApiClient, config *Config) (int, []dtrack.Finding) {
+	vulnerabilitiesCount, findings, err := findVulnerabilities(apiClient, config)
+	checkError(err)
+	if vulnerabilitiesCount > 0 {
+		fmt.Printf("%d vulnerabilities found!\n\n", vulnerabilitiesCount)
+		for _, f := range findings {
+			fmt.Print(formatFinding(f, apiClient))
+		}
+	}
+	return vulnerabilitiesCount, findings
 }
 
 func main() {
-	var inputFileName, projectId, apiKey, apiUrl, severityFilter, projectName, projectVersion string
-	var syncMode, autoCreateProject, useTeamCityOutput bool
-	var uploadResult dtrack.UploadResult
-	var timeout int
+	var err error
+	config := new(Config)
+	parseFlagsAndEnvs(config)
 
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Send SBOM file to Dependency Track for audit.\n\n")
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage of program:\n")
-		flag.PrintDefaults()
-		fmt.Fprintf(flag.CommandLine.Output(), "\nFields marked with (*) are required.\n")
-	}
+	apiClient := dtrack.ApiClient{ApiKey: config.apiKey, ApiUrl: config.apiUrl}
 
-	syncMode, err := strconv.ParseBool(os.Getenv("DTRACK_SYNC_MODE"))
-
-	if err != nil {
-		syncMode = false
-	}
-
-	autoCreateProject, err = strconv.ParseBool(os.Getenv("DTRACK_AUTO_CREATE_PROJECT"))
-	useTeamCityOutput, err = strconv.ParseBool(os.Getenv("DTRACK_TEAMCITY_OUTPUT"))
-
-	if err != nil {
-		autoCreateProject = false
-	}
-
-	flag.StringVar(&inputFileName, "i", "bom.xml", "Target SBOM file*")
-	flag.StringVar(&projectId, "p", os.Getenv("DTRACK_PROJECT_ID"), "Project ID. Environment variable is DTRACK_PROJECT_ID")
-	flag.StringVar(&projectName, "n", os.Getenv("DTRACK_PROJECT_NAME"), "Project name. It is used for auto creation of project. See option autoCreateProject for details. Environment variable is DTRACK_PROJECT_NAME")
-	flag.StringVar(&projectVersion, "v", os.Getenv("DTRACK_PROJECT_VERSION"), "Project version. It is used for auto creation of project. See option autoCreateProject for details. Environment variable is DTRACK_PROJECT_VERSION")
-	flag.StringVar(&apiKey, "k", os.Getenv("DTRACK_API_KEY"), "API Key*. Environment variable is DTRACK_API_KEY")
-	flag.StringVar(&apiUrl, "u", os.Getenv("DTRACK_API_URL"), "API URL*. Environment variable is DTRACK_API_URL")
-	flag.StringVar(&severityFilter, "g", os.Getenv("DTRACK_SEVERITY_FILTER"), "With Sync mode enabled show result and fail an audit if the results include a vulnerability with a severity of specified level or higher. Severity levels are: critical, high, medium, low, info, unassigned. Environment variable is DTRACK_SEVERITY_FILTER")
-	flag.BoolVar(&syncMode, "s", syncMode, "Sync mode enabled. That means: upload SBOM file, wait for scan result, show it and exit with non-zero code. Environment variable is DTRACK_SYNC_MODE")
-	flag.BoolVar(&autoCreateProject, "a", autoCreateProject, "Auto create project with projectName if it does not exist. Environment variable is DTRACK_AUTO_CREATE_PROJECT")
-	flag.BoolVar(&useTeamCityOutput, "T", useTeamCityOutput, "Use TeamCity output. Environment variable is DTRACK_TEAMCITY_OUTPUT")
-	flag.IntVar(&timeout, "t", 25, "Max timeout in second for polling API for project findings")
-	flag.Parse()
-
-	if apiKey == "" || apiUrl == "" {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	apiClient := dtrack.ApiClient{ApiKey: apiKey, ApiUrl: apiUrl}
-
-	if autoCreateProject && projectId == "" {
-		projectId, err = apiClient.LookupOrCreateProject(projectName, projectVersion)
+	if config.autoCreateProject && config.projectId == "" {
+		config.projectId, err = apiClient.LookupOrCreateProject(config.projectName, config.projectVersion)
 		checkError(err)
 	}
 
-	if projectId == "" {
+	if config.projectId == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	uploadResult, err = apiClient.Upload(inputFileName, projectId)
-	checkError(err)
-
-	if uploadResult.Token != "" {
-		fmt.Printf("SBOM file is successfully uploaded to DTrack API. Result token is %s\n", uploadResult.Token)
-	}
-
-	if uploadResult.Token != "" && syncMode {
-		if useTeamCityOutput {
-			printTeamCityMsg("run", "")
+	if config.syncMode {
+		var vulnerabilitiesCount int
+		if config.useTeamCityOutput {
+			vulnerabilitiesCount, _ = findAndPrintForTeamCity(apiClient, config)
+		} else {
+			vulnerabilitiesCount, _ = findAndPrintForUser(apiClient, config)
 		}
-		err := apiClient.PollTokenBeingProcessed(uploadResult.Token, time.After(time.Duration(timeout)*time.Second))
-		checkError(err)
-		findings, err := apiClient.GetFindings(projectId, severityFilter)
-		checkError(err)
-		if len(findings) > 0 {
-			fmt.Printf("%d vulnerabilities found!\n\n", len(findings))
-			for _, f := range findings {
-				if useTeamCityOutput {
-					printTeamCityMsg("output", formatFinding(f, apiClient))
-				} else {
-					fmt.Print(formatFinding(f, apiClient))
-				}
-			}
-			if useTeamCityOutput {
-				printTeamCityMsg("fail", "")
-			}
+		if vulnerabilitiesCount > 0 {
 			os.Exit(1)
-		}
-		if useTeamCityOutput {
-			printTeamCityMsg("pass", "")
 		}
 	}
 }
-
---
+++ cmd/dtrack-audit/teamcity.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"github.com/ozonru/dtrack-audit/internal/dtrack"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+const TeamCityPackageName = "github.com/ozonru/dtrack-audit/cmd/dtrack-audit"
+
+type Bom struct {
+	XMLName      xml.Name `xml:"bom"`
+	Text         string   `xml:",chardata"`
+	Xmlns        string   `xml:"xmlns,attr"`
+	Version      string   `xml:"version,attr"`
+	SerialNumber string   `xml:"serialNumber,attr"`
+	Components   struct {
+		Text      string `xml:",chardata"`
+		Component []struct {
+			Text                 string `xml:",chardata"`
+			Type                 string `xml:"type,attr"`
+			Name                 string `xml:"name"`
+			Version              string `xml:"version"`
+			Purl                 string `xml:"purl"`
+			HasVulnerabilities   bool
+			VulnerabilitiesDiscr string
+		} `xml:"component"`
+	} `xml:"components"`
+}
+
+type TeamCityMsg struct {
+	Time    time.Time
+	Action  string
+	Package string
+	Test    string
+	Output  string
+}
+
+func unmarshalXML(filePath string) []Bom {
+	result := make([]Bom, 0)
+	xmlFile, err := os.Open(filePath)
+	checkError(err)
+	defer xmlFile.Close()
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+	err = xml.Unmarshal(byteValue, &result)
+	checkError(err)
+	return result
+}
+
+func printTeamCityMsg(action, output, testName string) {
+	msg := TeamCityMsg{time.Now(), action, TeamCityPackageName, testName, output}
+	jsonData, err := json.Marshal(msg)
+	checkError(err)
+	fmt.Println(string(jsonData))
+}
+
+func populateBomWithFindings(bom []Bom, findings []dtrack.Finding, apiClient dtrack.ApiClient) []Bom {
+	for _, finding := range findings {
+		for _, v := range bom {
+			lib := v.Components.Component
+			for i := range lib {
+				if lib[i].Name == finding.Comp.Name && lib[i].Version == finding.Comp.Version {
+					lib[i].HasVulnerabilities = true
+					lib[i].VulnerabilitiesDiscr += formatFinding(finding, apiClient)
+					break
+				}
+			}
+		}
+	}
+	return bom
+}
+
+func findAndPrintForTeamCity(apiClient dtrack.ApiClient, config *Config) (int, []dtrack.Finding) {
+	bom := unmarshalXML(config.inputFileName)
+	vulnerabilitiesCount, findings, err := findVulnerabilities(apiClient, config)
+	checkError(err)
+	bom = populateBomWithFindings(bom, findings, apiClient)
+	for _, b := range bom {
+		libs := b.Components.Component
+		for i := range libs {
+			printTeamCityMsg("run", "", libs[i].Name)
+			if libs[i].HasVulnerabilities {
+				printTeamCityMsg("output", libs[i].VulnerabilitiesDiscr, libs[i].Name)
+				printTeamCityMsg("fail", "", libs[i].Name)
+			} else {
+				printTeamCityMsg("pass", "", libs[i].Name)
+			}
+		}
+	}
+	return vulnerabilitiesCount, findings
+}
